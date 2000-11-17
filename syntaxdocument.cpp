1c25ed539aadb4f63496292461964e417064c5f4
David Faure <faure@kde.org>
2000-11-17 12:10:33 +0000

    Well, we don't all have a /home/Maniac, Mr Maniac :-) locate("data", "kwrite/syntax.xml") is the way to go. Added better error handling (kdFatal is not very nice).

--- syntaxdocument.cpp
+++ syntaxdocument.cpp
@@ -18,24 +18,25 @@
 #include "syntaxdocument.h"
 #include <qfile.h>
 #include <kdebug.h>
-//#define TESTING
+#include <kstddirs.h>
+#include <klocale.h>
+#include <kmessagebox.h>
+
 SyntaxDocument::SyntaxDocument() : QDomDocument()
 {
-#ifdef TESTING
-  QFile f("/home/Maniac/kwrite/kwrite/syntax.xml");
-#else
-  QFile f("syntax.xml");
-#endif
-	if( f.open(IO_ReadOnly) ) {
-	setContent(&f);
-  f.close();
-	} else {
-  // TODO error handling
-	kdFatal() << "OOPS File not found" << endl;
-	f.close();
-  }
-
+    QString syntaxPath = locate("data", "kwrite/syntax.xml");
+    if( !syntaxPath.isEmpty() )
+    {
+        QFile f( syntaxPath );
+        if ( f.open(IO_ReadOnly) )
+            setContent(&f);
+        else
+            KMessageBox::error( 0L, i18n("Can't open %1").arg(syntaxPath) );
+        f.close();
+    } else
+        KMessageBox::error( 0L, i18n("File share/apps/kwrite/syntax.xml not found ! Check your installation!") );
 }
+
 SyntaxDocument::~SyntaxDocument()
 {
 
@@ -54,34 +55,34 @@ SyntaxDocument::~SyntaxDocument()
 QStringList& SyntaxDocument::finddata(const QString& langName,const QString& type)
 {
 //	kdDebug() << "Language is " << langName << "\tType is " << type << endl;
-  QDomElement docElem = documentElement();
-  QDomNode n = docElem.firstChild();
-  while ( !n.isNull() ) {
-      if ( n.isElement()) {
-          QDomElement e = n.toElement(); //e.tagName is language
-					QDomNode child=e.firstChild(); // child.toElement().tagname() is keywords/types
- 					QDomNode grandchild=child.firstChild(); // grandchild.tagname is keyword/type
-          QDomElement e1=grandchild.toElement();  // convience pointer
+    QDomElement docElem = documentElement();
+    QDomNode n = docElem.firstChild();
+    while ( !n.isNull() ) {
+        if ( n.isElement()) {
+            QDomElement e = n.toElement(); //e.tagName is language
+            QDomNode child=e.firstChild(); // child.toElement().tagname() is keywords/types
+            QDomNode grandchild=child.firstChild(); // grandchild.tagname is keyword/type
+            QDomElement e1=grandchild.toElement();  // convience pointer
 // at this point e.attribute("name") should equal langName
 //    and we need the grandchild.tagName() equal to type
 //          kdDebug() << e.attribute("name") << "\t\t" << grandchild.toElement().tagName() << "\t" << langName << " " << type << endl;
-					if(e.attribute("name").compare(langName)==0 && grandchild.toElement().tagName().compare(type) == 0 ){
-              QDomNodeList childlist=n.childNodes();
-              QDomNode childnode=childlist.item(0).firstChild();
-              QDomNodeList grandchildlist=childlist.item(0).childNodes();
+            if(e.attribute("name").compare(langName)==0 && grandchild.toElement().tagName().compare(type) == 0 ){
+                QDomNodeList childlist=n.childNodes();
+                QDomNode childnode=childlist.item(0).firstChild();
+                QDomNodeList grandchildlist=childlist.item(0).childNodes();
 //							kdDebug() << "Count of grandchildlist and tagname " << grandchildlist.count() <<"\t" << grandchildlist.item(0).toElement().tagName() << endl;
 //							kdDebug() << childnode.toElement().tagName() <<"\tcount " << childlist.count() << endl;
 //              kdDebug() << childlist.item(0).toElement().tagName() <<"\t" childlist.item(1).toElement().tagName() << endl;
-              for(uint i=0; i< grandchildlist.count();i++) {
-              m_data+=grandchildlist.item(i).toElement().text().stripWhiteSpace();
+                for(uint i=0; i< grandchildlist.count();i++) {
+                    m_data+=grandchildlist.item(i).toElement().text().stripWhiteSpace();
 //						  childnode=childnode.nextSibling();
-						}
+                }
 //          m_data=QStringList::split(" ",grandchild.toElement().text());
 //          kdDebug() << m_data.count()<< endl;
-          return m_data;
-					}
-      }
-      n = n.nextSibling();
-  }
-	return m_data;
+                return m_data;
+            }
+        }
+        n = n.nextSibling();
+    }
+    return m_data;
 }
