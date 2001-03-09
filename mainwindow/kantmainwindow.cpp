47fa5fdb5d8a37eb1738b3f38cab0338049de531
Torsten Rahn <tackat@kde.org>
2001-03-09 15:09:01 +0000

    Changed iconname

--- mainwindow/kantmainwindow.cpp
+++ mainwindow/kantmainwindow.cpp
@@ -261,7 +261,7 @@ void KantMainWindow::setupActions()
   SLOT( slotEditFilter() ), actionCollection(), "edit_filter" );
   viewSplitVert = new KAction( i18n("Split &Vertical"), "view_left_right", CTRL+SHIFT+Key_L, viewManager, SLOT( slotSplitViewSpaceVert() ), actionCollection(), "view_split_vert");
   viewSplitHoriz = new KAction( i18n("Split &Horizontal"), "view_top_bottom", CTRL+SHIFT+Key_T, viewManager, SLOT( slotSplitViewSpaceHoriz() ), actionCollection(), "view_split_horiz");
-  closeCurrentViewSpace = new KAction( i18n("Close &Current"), "remove_view", CTRL+SHIFT+Key_R, viewManager, SLOT( slotCloseCurrentViewSpace() ), actionCollection(), "view_close_current_space");
+  closeCurrentViewSpace = new KAction( i18n("Close &Current"), "view_remove", CTRL+SHIFT+Key_R, viewManager, SLOT( slotCloseCurrentViewSpace() ), actionCollection(), "view_close_current_space");
 
   goNext=new KAction(i18n("Next View"),Key_F8,viewManager, SLOT(activateNextView()),actionCollection(),"go_next");
   goPrev=new KAction(i18n("Previous View"),SHIFT+Key_F8,viewManager, SLOT(activatePrevView()),actionCollection(),"go_prev");
