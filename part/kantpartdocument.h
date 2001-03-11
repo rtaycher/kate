7d718c0050ef91801cfba8f360d0ea940310b2be
Thomas Leitner <tom@radar.tu-graz.ac.at>
2001-03-11 21:15:28 +0000

    need a variable type for m_bSingleViewMode

--- part/kantpartdocument.h
+++ part/kantpartdocument.h
@@ -29,7 +29,7 @@ class KantPartDocument : public KantDocument
     ~KantPartDocument ();
 
   private:
-    m_bSingleViewMode;
+    bool m_bSingleViewMode;
 };
 
 class KantPartBrowserExtension : public KParts::BrowserExtension
