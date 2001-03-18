68e7cdd3481de87ca408f925d1a5c69944d3169e
Christoph Cullmann <cullmann@kde.org>
2001-03-18 11:53:27 +0000

    copyright added

--- pluginmanager/kantconfigplugindialogpage.cpp
+++ pluginmanager/kantconfigplugindialogpage.cpp
@@ -1,3 +1,20 @@
+/***************************************************************************
+                          kantconfigplugindialogpage.cpp  -  description
+                             -------------------
+    begin                : FRE Feb 23 2001
+    copyright            : (C) 2001 by Joseph Wenninger
+    email                : jowenn@bigfoot.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #include "kantconfigplugindialogpage.h"
 #include "kantconfigplugindialogpage.moc"
 
--- pluginmanager/kantconfigplugindialogpage.h
+++ pluginmanager/kantconfigplugindialogpage.h
@@ -1,3 +1,20 @@
+/***************************************************************************
+                          kantconfigplugindialogpage.h  -  description
+                             -------------------
+    begin                : FRE Feb 23 2001
+    copyright            : (C) 2001 by Joseph Wenninger
+    email                : jowenn@bigfoot.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #ifndef _kant_config_Plugin_Dialog_Page_h_
 #define _kant_config_Plugin_Dialog_Page_h_
 
