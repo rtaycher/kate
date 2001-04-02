4a9f33e77f777a2523a45080b1c4e44f478bcfe8
Werner Trobin <trobin@kde.org>
2001-04-02 17:48:07 +0000

    I added a tiny Perl script to get the strings out of the XML files to make them i18n-able. The script works, but I'm sure I screwed up the messages: target in Makefile.am (Thank you in advance, Coolo :)

---
+++ data/preparedata.pl
@@ -0,0 +1,25 @@
+#!/usr/local/bin/perl -w
+# little script to extract the text from the .xml files
+# and output it, so xgettext can add the tips to the po file
+#
+# 2000 by Matthias Kiefer <matthias.kiefer@gmx.de>
+
+if($#ARGV<0) {
+    die "Usage: perl preparedata.pl *.xml\n";
+}
+
+open(OUT,">data.cpp") || die "Unable to open the output file (data.cpp)";
+print OUT "// This file is generated on make messages and it makes it possible\n";
+print OUT "// to translate the strings in the XML files in this directory.\n\n";
+
+foreach(@ARGV) {
+    open(FILE,"<$_") || die "Unable to open ", $_;
+    while(<FILE>) {
+        if(m/.*itemData\s+name=\"(.*?)\"/) {
+            print OUT "i18n( \"", $1, "\" );\n";
+        }
+    }
+    close(FILE);
+}
+print OUT "\n"; # final newline :)
+close(OUT);
