85431bfebacec3c8136cb2975ee1df10da4a8566
Christoph Cullmann <cullmann@kde.org>
2001-04-20 16:53:42 +0000

    enhanced plugininterface to provide configpages for each plugin

--- interfaces/katepluginIface.h
+++ interfaces/katepluginIface.h
@@ -23,6 +23,24 @@
 #include <kxmlguiclient.h>
 #include <qlist.h>
 #include <qstring.h>
+#include <qwidget.h>
+
+class KatePluginConfigPageIface : public QWidget
+{
+  Q_OBJECT
+
+  friend class KatePluginIface;
+
+  public:
+    KatePluginConfigPageIface (QObject* parent = 0L, QWidget *parentWidget=0L) : QWidget (parentWidget, 0L)
+     { pluginIface = (KatePluginIface *) parent; };
+
+    ~KatePluginConfigPageIface () {;};
+
+    virtual void applyConfig () { ; };
+
+    class KatePluginIface *pluginIface;;
+};
 
 class KatePluginViewIface : public QObject, virtual public KXMLGUIClient
 {
@@ -31,33 +49,36 @@ class KatePluginViewIface : public QObject, virtual public KXMLGUIClient
   friend class KatePluginIface;
 
   public:
-    KatePluginViewIface (QObject* parent = 0) : QObject (0L, 0L)
+    KatePluginViewIface (QObject* parent = 0L) : QObject (0L, 0L)
      { pluginIface = (KatePluginIface *) parent; };
 
     ~KatePluginViewIface () {;};
 
     void setXML (QString filename)
-      { setXMLFile( filename ); }
+      { setXMLFile( filename ); };
 
-      class KatePluginIface *pluginIface;;
+    class KatePluginIface *pluginIface;;
 };
 
 class KatePluginIface : public QObject
 {
   Q_OBJECT
 
-  friend class KatePluginViewIface;
+  friend KatePluginViewIface;
+  friend KatePluginConfigPageIface;
 
   public:
-    KatePluginIface (QObject* parent = 0, const char* name = 0) : QObject (parent, name)
+    KatePluginIface (QObject* parent = 0L, const char* name = 0L) : QObject (parent, name)
     { appIface = (KateAppIface *) parent; };
 
     virtual ~KatePluginIface () {;};
 
     virtual KatePluginViewIface *createView ()=0;
 
-    QList<KatePluginViewIface> viewList;
+    virtual KatePluginConfigPageIface *createConfigPage (QWidget *) { return 0L; };
+    virtual bool hasConfigPage() { return false; };
 
+    QList<KatePluginViewIface> viewList;
     KateAppIface *appIface;
 };
 
