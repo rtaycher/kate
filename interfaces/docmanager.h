bfc26bc07a2d970e0850839d18bf1b4292d21fc5
Christoph Cullmann <cullmann@kde.org>
2001-05-15 18:34:39 +0000

    now the kate plugin interfaces are LGPL - for non GPL plugins

--- interfaces/docmanager.h
+++ interfaces/docmanager.h
@@ -7,12 +7,20 @@
  ***************************************************************************/
 
 /***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
  ***************************************************************************/
 
 #ifndef _KATE_DOCMANAGER_INCLUDE_
@@ -29,23 +37,23 @@ class DocManager : public QObject
   Q_OBJECT
 
   public:
-     DocManager ();
+    DocManager ();
     virtual ~DocManager ();
 
   public:
     // get doc number n/current/first/next/doc by id
     virtual class Document *getNthDoc (uint) { return 0L; };
     virtual class Document *getCurrentDoc () { return 0L; };
-     virtual class Document *getFirstDoc () { return 0L; };
-     virtual class Document *getNextDoc () { return 0L; };
-     virtual class Document *getDocWithID (uint) { return 0L; };
+    virtual class Document *getFirstDoc () { return 0L; };
+    virtual class Document *getNextDoc () { return 0L; };
+    virtual class Document *getDocWithID (uint) { return 0L; };
 
-     // find do with URL / is doc with URL open ?
-     virtual int findDoc (KURL) { return 0L; };
-     virtual bool isOpen (KURL) { return 0L; };
+    // find do with URL / is doc with URL open ?
+    virtual int findDoc (KURL) { return 0L; };
+    virtual bool isOpen (KURL) { return 0L; };
 
-     // how much docs open ?
-     virtual uint docCount () { return 0L; };
+    // how much docs open ?
+    virtual uint docCount () { return 0L; };
 };
 
 };
