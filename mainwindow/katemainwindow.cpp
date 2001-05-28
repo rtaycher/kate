ee4dbb57b3acc931d5211d3abf18828bfcd7d288
Joseph Wenninger <kde@jowenn.at>
2001-05-28 20:24:49 +0000

    Compile with new kdockwidget features if available

--- mainwindow/katemainwindow.cpp
+++ mainwindow/katemainwindow.cpp
@@ -144,12 +144,13 @@ void KateMainWindow::setupMainWindow ()
   fileselector->dirOperator()->setView(KFile::Simple);
   fileselectorDock->setWidget (fileselector);
 
-  // disabled because it will be only available in KDE >= 2.2
-  /*filelistDock->setDockWindowType (NET::Tool);
+#ifdef _KDOCKWIDGET_2_2_
+//  disabled because it will be only available in KDE >= 2.2
+  filelistDock->setDockWindowType (NET::Tool);
   fileselectorDock->setDockWindowType (NET::Tool);
   filelistDock->setDockWindowTransient (true);
-  fileselectorDock->setDockWindowTransient (true);*/
-
+  fileselectorDock->setDockWindowTransient (true);
+#endif
   connect(fileselector->dirOperator(),SIGNAL(fileSelected(const KFileViewItem*)),this,SLOT(fileSelected(const KFileViewItem*)));
 
   mainDock->setEnableDocking ( KDockWidget::DockNone );
