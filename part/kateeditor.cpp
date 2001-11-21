d3e60106f38f8224372d7aca6e8967bb2150e2c9
Christoph Cullmann <cullmann@kde.org>
2001-11-21 21:51:32 +0000

    KTextEditor::Editor, first try

---
+++ part/kateeditor.cpp
@@ -0,0 +1,29 @@
+/***************************************************************************
+                          katefactory.cpp  -  description
+                             -------------------
+    begin                : Mon Feb 5 2001
+    copyright            : (C) 2001 by Christoph Cullmann
+    email                : cullmann@kde.org
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "kateeditor.moc"
+
+
+KateEditor::KateEditor(QWidget *parentWidget, const char *widgetName, QObject *parent, const char *name)
+{
+
+}
+
+KateEditor::~KateEditor ()
+{
+
+}
