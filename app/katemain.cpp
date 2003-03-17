1cf5c68a60302d7dbe0275732136b589f0c3d2e0
Christoph Cullmann <cullmann@kde.org>
2003-03-17 21:28:12 +0000

    sync about dialogs

--- app/katemain.cpp
+++ app/katemain.cpp
@@ -42,45 +42,46 @@ static KCmdLineOptions options[] =
 
 int main( int argc, char **argv )
 {
-  KAboutData *data = new KAboutData  ("kate", I18N_NOOP("Kate"), "2.2",
-                                                           I18N_NOOP( "Kate - KDE Advanced Text Editor" ), KAboutData::License_LGPL_V2,
+  KAboutData *s_about = new KAboutData  ("kate", I18N_NOOP("Kate"), "2.2",
+                                                           I18N_NOOP( "Kate - Advanced Text Editor" ), KAboutData::License_LGPL_V2,
                                                            I18N_NOOP( "(c) 2000-2003 The Kate Authors" ), 0, "http://kate.kde.org");
 
-  data->addAuthor ("Christoph Cullmann", I18N_NOOP("Maintainer"), "cullmann@kde.org", "http://www.babylon2k.de");
-  data->addAuthor ("Anders Lund", I18N_NOOP("Core Developer"), "anders@alweb.dk", "http://www.alweb.dk");
-  data->addAuthor ("Joseph Wenninger", I18N_NOOP("Core Developer"), "jowenn@kde.org","http://stud3.tuwien.ac.at/~e9925371");
-  data->addAuthor ("Michael Bartl", I18N_NOOP("Core Developer"), "michael.bartl1@chello.at");
-  data->addAuthor ("Matt Newell", I18N_NOOP("Testing, ..."), "newellm@proaxis.com");
-  data->addAuthor ("Charles Samuels", I18N_NOOP("Core Developer"), "charles@kde.org");
-  data->addAuthor ("Michael McCallum", I18N_NOOP("Core Developer"), "gholam@xtra.co.nz");
-  data->addAuthor ("Jochen Wilhemly", I18N_NOOP( "KWrite Author" ), "digisnap@cs.tu-berlin.de" );
-  data->addAuthor ("Michael Koch",I18N_NOOP("KWrite port to KParts"), "koch@kde.org");
-  data->addAuthor ("Christian Gebauer", 0, "gebauer@kde.org" );
-  data->addAuthor ("Simon Hausmann", 0, "hausmann@kde.org" );
-  data->addAuthor ("Glen Parker",I18N_NOOP("KWrite Undo History, Kspell integration"), "glenebob@nwlink.com");
-  data->addAuthor ("Scott Manson",I18N_NOOP("KWrite XML Syntax highlighting support"), "sdmanson@alltel.net");
-  data->addAuthor ("John Firebaugh",I18N_NOOP("Patches and more"), "jfirebaugh@kde.org");
-  data->addAuthor ("Hamish Rodda",I18N_NOOP("Patches and more"), "meddie@yoyo.its.monash.edu.au");
+    s_about->addAuthor ("Christoph Cullmann", I18N_NOOP("Maintainer"), "cullmann@kde.org", "http://www.babylon2k.de");
+    s_about->addAuthor ("Anders Lund", I18N_NOOP("Core Developer"), "anders@alweb.dk", "http://www.alweb.dk");
+    s_about->addAuthor ("Joseph Wenninger", I18N_NOOP("Core Developer"), "jowenn@kde.org","http://stud3.tuwien.ac.at/~e9925371");
+    s_about->addAuthor ("Hamish Rodda",I18N_NOOP("Core Developer"), "meddie@yoyo.its.monash.edu.au");
+    s_about->addAuthor ("Waldo Bastian", I18N_NOOP( "The cool buffersystem" ), "bastian@kde.org" );
+    s_about->addAuthor ("Charles Samuels", I18N_NOOP("The Editing Commands"), "charles@kde.org");
+    s_about->addAuthor ("Matt Newell", I18N_NOOP("Testing, ..."), "newellm@proaxis.com");
+    s_about->addAuthor ("Michael Bartl", I18N_NOOP("Former Core Developer"), "michael.bartl1@chello.at");
+    s_about->addAuthor ("Michael McCallum", I18N_NOOP("Core Developer"), "gholam@xtra.co.nz");
+    s_about->addAuthor ("Jochen Wilhemly", I18N_NOOP( "KWrite Author" ), "digisnap@cs.tu-berlin.de" );
+    s_about->addAuthor ("Michael Koch",I18N_NOOP("KWrite port to KParts"), "koch@kde.org");
+    s_about->addAuthor ("Christian Gebauer", 0, "gebauer@kde.org" );
+    s_about->addAuthor ("Simon Hausmann", 0, "hausmann@kde.org" );
+    s_about->addAuthor ("Glen Parker",I18N_NOOP("KWrite Undo History, Kspell integration"), "glenebob@nwlink.com");
+    s_about->addAuthor ("Scott Manson",I18N_NOOP("KWrite XML Syntax highlighting support"), "sdmanson@alltel.net");
+    s_about->addAuthor ("John Firebaugh",I18N_NOOP("Patches and more"), "jfirebaugh@kde.org");
 
-  data->addCredit ("Matteo Merli",I18N_NOOP("Highlighting for RPM Spec-Files, Perl, Diff and more"), "merlim@libero.it");
-  data->addCredit ("Rocky Scaletta",I18N_NOOP("Highlighting for VHDL"), "rocky@purdue.edu");
-  data->addCredit ("Yury Lebedev",I18N_NOOP("Highlighting for SQL"),"");
-  data->addCredit ("Chris Ross",I18N_NOOP("Highlighting for Ferite"),"");
-  data->addCredit ("Nick Roux",I18N_NOOP("Highlighting for ILERPG"),"");
-  data->addCredit ("Carsten Niehaus", I18N_NOOP("Highlighting for LaTeX"),"");
-  data->addCredit ("Per Wigren", I18N_NOOP("Highlighting for Makefiles, Python"),"");
-  data->addCredit ("Jan Fritz", I18N_NOOP("Highlighting for Python"),"");
-  data->addCredit ("Daniel Naber","","");
-  data->addCredit ("Roland Pabel",I18N_NOOP("Highlighting for Scheme"),"");
-  data->addCredit ("Cristi Dumitrescu",I18N_NOOP("PHP Keyword/Datatype list"),"");
-  data->addCredit ("Carsten Presser", I18N_NOOP("Betatest"), "mord-slime@gmx.de");
-  data->addCredit ("Jens Haupert", I18N_NOOP("Betatest"), "al_all@gmx.de");
-  data->addCredit ("Carsten Pfeiffer", I18N_NOOP("Very nice help"), "");
-  data->addCredit (I18N_NOOP("All people who have contributed and I have forgotten to mention"),"","");
+    s_about->addCredit ("Matteo Merli",I18N_NOOP("Highlighting for RPM Spec-Files, Perl, Diff and more"), "merlim@libero.it");
+    s_about->addCredit ("Rocky Scaletta",I18N_NOOP("Highlighting for VHDL"), "rocky@purdue.edu");
+    s_about->addCredit ("Yury Lebedev",I18N_NOOP("Highlighting for SQL"),"");
+    s_about->addCredit ("Chris Ross",I18N_NOOP("Highlighting for Ferite"),"");
+    s_about->addCredit ("Nick Roux",I18N_NOOP("Highlighting for ILERPG"),"");
+    s_about->addCredit ("Carsten Niehaus", I18N_NOOP("Highlighting for LaTeX"),"");
+    s_about->addCredit ("Per Wigren", I18N_NOOP("Highlighting for Makefiles, Python"),"");
+    s_about->addCredit ("Jan Fritz", I18N_NOOP("Highlighting for Python"),"");
+    s_about->addCredit ("Daniel Naber","","");
+    s_about->addCredit ("Roland Pabel",I18N_NOOP("Highlighting for Scheme"),"");
+    s_about->addCredit ("Cristi Dumitrescu",I18N_NOOP("PHP Keyword/Datatype list"),"");
+    s_about->addCredit ("Carsten Presser", I18N_NOOP("Betatest"), "mord-slime@gmx.de");
+    s_about->addCredit ("Jens Haupert", I18N_NOOP("Betatest"), "al_all@gmx.de");
+    s_about->addCredit ("Carsten Pfeiffer", I18N_NOOP("Very nice help"), "");
+    s_about->addCredit (I18N_NOOP("All people who have contributed and I have forgotten to mention"),"","");
 
-  data->setTranslator(I18N_NOOP("_: NAME OF TRANSLATORS\nYour names"), I18N_NOOP("_: EMAIL OF TRANSLATORS\nYour emails"));
+    s_about->setTranslator(I18N_NOOP("_: NAME OF TRANSLATORS\nYour names"), I18N_NOOP("_: EMAIL OF TRANSLATORS\nYour emails"));
 
-  KCmdLineArgs::init (argc, argv, data);
+  KCmdLineArgs::init (argc, argv, s_about);
   KCmdLineArgs::addCmdLineOptions (options);
   KateApp::addCmdLineOptions ();
   KCmdLineArgs* args = KCmdLineArgs::parsedArgs();
@@ -93,7 +94,7 @@ int main( int argc, char **argv )
 
   if (newProcess)
   {
-    KInstance *instance = new KInstance (data);
+    KInstance *instance = new KInstance (s_about);
 
     KConfig *config = instance->config();
     config->setGroup("KDE");
