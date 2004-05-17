59c12234d12e684d95c33579a26fc65f29dc5066
Stephan Binner <binner@kde.org>
2004-05-17 11:37:45 +0000

    setButtonGuiItem++

--- app/katemailfilesdialog.cpp
+++ app/katemailfilesdialog.cpp
@@ -49,7 +49,7 @@ KateMailDialog::KateMailDialog( QWidget *parent, KateMainWindow  *mainwin )
                 KGuiItem( i18n("&Show All Documents >>") ) ),
     mainWindow( mainwin )
 {
-  setButtonText( KDialogBase::Ok, i18n("&Mail...") );
+  setButtonGuiItem( KDialogBase::Ok, KGuiItem( i18n("&Mail..."), "mail_send") );
   mw = makeVBoxMainWidget();
   mw->installEventFilter( this );
 
