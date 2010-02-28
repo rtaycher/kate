ce91d373d8cb043510f3c4ed9c626c7db9552f4d
Jonathan Schmidt-Dominé <jonathan@KDE-GNU-Linux.site>
2010-02-28 16:15:15 +0100

    Relicensed AComment as LGPLv3+

--- playground/kte_acomment/acommentplugin.h
+++ playground/kte_acomment/acommentplugin.h
@@ -1,17 +1,22 @@
 /****************************************************************************
- **                - Artistic Comment KTextEditor-Plugin -                 **
- ** - Copyright (C) 2010 by Jonathan Schmidt-Dominé <devel@the-user.org> - **
+ **    - This file is part of the Artistic Comment KTextEditor-Plugin -    **
+ **  - Copyright 2009-2010 Jonathan Schmidt-Dominé <devel@the-user.org> -  **
  **                                  ----                                  **
- **   - This program is free software: you can redistribute it and/or -    **
- **   - modify it under the terms of the GNU General Public License as -   **
- ** - published by the Free Software Foundation, either version 2 of the - **
- **          - License, or (at your option) any later version. -           **
- **  - This program is distributed in the hope that it will be useful, -   **
+ **   - This program is free software; you can redistribute it and/or -    **
+ **    - modify it under the terms of the GNU Library General Public -     **
+ **    - License version 3, or (at your option) any later version, as -    **
+ ** - published by the Free Software Foundation.                         - **
+ **                                  ----                                  **
+ **  - This library is distributed in the hope that it will be useful, -   **
  **   - but WITHOUT ANY WARRANTY; without even the implied warranty of -   **
  ** - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU -  **
- **              - General Public License for more details. -              **
- ** - You should have received a copy of the GNU General Public License -  **
- **   - along with this program. If not, see <http://gnu.org/licenses> -   **
+ **          - Library General Public License for more details. -          **
+ **                                  ----                                  **
+ ** - You should have received a copy of the GNU Library General Public -  **
+ ** - License along with the kdelibs library; see the file COPYING.LIB. -  **
+ **  - If not, write to the Free Software Foundation, Inc., 51 Franklin -  **
+ **      - Street, Fifth Floor, Boston, MA 02110-1301, USA. or see -       **
+ **                  - <http://www.gnu.org/licenses/>. -                   **
  ***************************************************************************/
 
 #ifndef ACOMMENTPLUGIN_H
