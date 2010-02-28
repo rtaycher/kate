9434b647117b6a402e415ced0e61f4100405365f
Christoph Cullmann <cullmann@kde.org>
2010-02-28 17:51:12 +0100

    poison the textline with nearly all old stuff, beside the text modifications, which now must be done over buffer this is needed to be able to use it as drop-in replacement in kate part later must be cleaned up again :(

--- playground/katetextline.h
+++ playground/katetextline.h
@@ -21,6 +21,7 @@
 #ifndef KATE_TEXTLINE_H
 #define KATE_TEXTLINE_H
 
+#include <QtCore/QVector>
 #include <QtCore/QString>
 #include <QtCore/QSharedPointer>
 
@@ -39,6 +40,18 @@ class TextLineData {
   friend class TextBlock;
 
   public:
+    /**
+     * Flags of TextLineData
+     */
+    enum Flags
+    {
+      flagHlContinue = 1,
+      flagAutoWrapped = 2,
+      flagFoldingColumnsOutdated = 4,
+      flagNoIndentationBasedFolding = 8,
+      flagNoIndentationBasedFoldingAtStart = 16
+    };
+
     /**
      * Construct an empty text line.
      */
@@ -110,10 +123,229 @@ class TextLineData {
       return QChar();
     }
 
+    /**
+     * Set the flag that only positions have changed, not folding region begins/ends themselve
+     * @param set folding columns our of date?
+     */
+    void setFoldingColumnsOutdated(bool set)
+    {
+      if (set) m_flags |= flagFoldingColumnsOutdated;
+      else m_flags &= (~flagFoldingColumnsOutdated);
+    }
+
+    /**
+     * Returns \e true, if the folding columns are outdated, otherwise returns \e false.
+     * @return folding columns our of date?
+     */
+    bool foldingColumnsOutdated() const { return m_flags & flagFoldingColumnsOutdated; }
+
+    /**
+     * Returns the line's length.
+     */
+    int length() const { return m_text.length(); }
+
+    /**
+     * Returns \e true, if the line's hl-continue flag is set, otherwise returns
+     * \e false. The hl-continue flag is set in the hl-definition files.
+     * @return hl-continue flag is set
+     */
+    bool hlLineContinue () const { return m_flags & flagHlContinue; }
+
+    /**
+     * Returns \e true, if the line was automagically wrapped, otherwise returns
+     * \e false.
+     * @return was this line auto-wrapped?
+     */
+    bool isAutoWrapped () const { return m_flags & flagAutoWrapped; }
+
+    /**
+     * Returns the complete text line (as a QString reference).
+     * @return text of this line, read-only
+     */
+    const QString& string() const { return m_text; }
+
+    /**
+     * Returns the substring with \e length beginning at the given \e column.
+     * @param column start column of text to return
+     * @param length length of text to return
+     * @return wanted part of text
+     */
+    QString string (int column, int length) const
+    { return m_text.mid(column, length); }
+
+    /**
+     * Leading whitespace of this line
+     * @return leading whitespace of this line
+     */
+    QString leadingWhitespace() const;
+
+    /**
+     * Returns the indentation depth with each tab expanded into \e tabWidth characters.
+     */
+    int indentDepth (int tabWidth) const;
+
+    /**
+     * Returns the \e column with each tab expanded into \e tabWidth characters.
+     */
+    int toVirtualColumn (int column, int tabWidth) const;
+
+    /**
+     * Returns the "real" column where each tab only counts one character.
+     * The conversion calculates with \e tabWidth characters for each tab.
+     */
+    int fromVirtualColumn (int column, int tabWidth) const;
+
+    /**
+     * Returns the text length with each tab expanded into \e tabWidth characters.
+     */
+    int virtualLength (int tabWidth) const;
+
+    /**
+     * Returns \e true, if \e match equals to the text at position \e column,
+     * otherwise returns \e false.
+     */
+    bool matchesAt(int column, const QString& match) const;
+
+    /**
+     * Returns \e true, if the line starts with \e match, otherwise returns \e false.
+     */
+    bool startsWith(const QString& match) const { return m_text.startsWith (match); }
+
+    /**
+     * Returns \e true, if the line ends with \e match, otherwise returns \e false.
+     */
+    bool endsWith(const QString& match) const { return m_text.endsWith (match); }
+
+    /**
+     * Gets the attribute at the given position
+     * use KRenderer::attributes  to get the KTextAttribute for this.
+     *
+     * @param pos position of attribute requested
+     * @return value of attribute
+     */
+    int attribute (int pos) const
+    {
+      for (int i=0; i < m_attributesList.size(); i+=3)
+      {
+        if (pos >= m_attributesList[i] && pos < m_attributesList[i]+m_attributesList[i+1])
+          return m_attributesList[i+2];
+
+        if (pos < m_attributesList[i])
+          break;
+      }
+
+      return 0;
+    }
+
+    /**
+     * context stack
+     * @return context stack
+     */
+    const QVector<short> &ctxArray () const { return m_ctx; }
+
+    /**
+     * @return true if any context at the line end has the noIndentBasedFolding flag set
+     */
+    bool noIndentBasedFolding() const { return m_flags & flagNoIndentationBasedFolding; }
+
+    /**
+     * @return true if any context at the line end has the noIndentationBasedFoldingAtStart flag set
+     */
+    bool noIndentBasedFoldingAtStart() const { return m_flags & flagNoIndentationBasedFoldingAtStart; }
+
+    /**
+     * folding list
+     * @return folding array
+     */
+    const QVector<int> &foldingListArray () const { return m_foldingList; }
+
+    /**
+     * indentation stack
+     * @return indentation array
+     */
+    const QVector<unsigned short> &indentationDepthArray () const { return m_indentationDepth; }
+
+    /**
+     * Add attribute for given start + length to this line
+     * @param start start column of this attribute
+     * @param length length in chars this attribute should span
+     * @param attribute attribute to use
+     */
+    void addAttribute (int start, int length, int attribute);
+
+    /**
+     * Clear attributes of this line
+     */
+    void clearAttributes () { m_attributesList.clear (); }
+
+    /**
+     * Accessor to attributes
+     * @return attributes of this line
+     */
+    const QVector<int> &attributesList () const { return m_attributesList; }
+
+    /**
+     * set hl continue flag
+     * @param cont continue flag?
+     */
+    void setHlLineContinue (bool cont)
+    {
+      if (cont) m_flags = m_flags | flagHlContinue;
+      else m_flags = m_flags & ~ flagHlContinue;
+    }
+
+    /**
+     * set auto-wrapped property
+     * @param wrapped line was wrapped?
+     */
+    void setAutoWrapped (bool wrapped)
+    {
+      if (wrapped) m_flags = m_flags | flagAutoWrapped;
+      else m_flags = m_flags & ~ flagAutoWrapped;
+    }
+
+    /**
+     * Sets the syntax highlight context number
+     * @param val new context array
+     */
+    void setContext (QVector<short> &val) { m_ctx = val; }
+
+    /**
+     * sets if for the next line indent based folding should be disabled
+     * @param val should indent folding be disabled
+     */
+    void setNoIndentBasedFolding(bool val)
+    {
+      if (val) m_flags = m_flags | flagNoIndentationBasedFolding;
+      else m_flags = m_flags & ~ flagNoIndentationBasedFolding;
+    }
+
+    /**
+     * sets if indent based folding should be disabled at start
+     * @param val should indent based folding be disabled at start
+     */
+    void setNoIndentBasedFoldingAtStart(bool val)
+    {
+      if (val) m_flags = m_flags | flagNoIndentationBasedFoldingAtStart;
+      else m_flags = m_flags & ~ flagNoIndentationBasedFoldingAtStart;
+    }
+
+    /**
+     * update folding list
+     * @param val new folding list
+     */
+    void setFoldingList (QVector<int> &val) { m_foldingList = val; }
+
+    /**
+     * update indentation stack
+     * @param val new indentation stack
+     */
+    void setIndentationDepth (QVector<unsigned short> &val) { m_indentationDepth = val; }
+
   private:
     /**
      * Accessor to the text contained in this line.
-     * This accessor is private, only the friend class text buffer is allowed to access the text read/write.
+     * This accessor is private, only the friend class text buffer/block is allowed to access the text read/write.
      * @return text of this line
      */
     QString &textReadWrite () { return m_text; }
@@ -123,6 +355,32 @@ class TextLineData {
      * text of this line
      */
     QString m_text;
+
+    /**
+     * store the attribs, int array
+     * one int start, next one len, next one attrib
+     */
+    QVector<int> m_attributesList;
+
+    /**
+     * context stack
+     */
+    QVector<short> m_ctx;
+
+    /**
+     * list of folding starts/ends
+     */
+    QVector<int> m_foldingList;
+
+    /**
+     * indentation stack
+     */
+    QVector<unsigned short> m_indentationDepth;
+
+    /**
+     * flags
+     */
+    uchar m_flags;
 };
 
 /**
