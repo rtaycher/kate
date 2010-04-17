rtaycher/kate

commit f3f075c46a61ecc3695dfeb7a05f0c5d6179de13

fix bug in backward regex search
BUG: 227459

svn path=/trunk/KDE/kdelibs/kate/; revision=1115422

part/search/kateregexpsearch.cpp
@@ -207,8 +207,6 @@ QVector<KTextEditor::Range> KateRegExpSearch::search(
   // regex search
   KateRegExp regexp(pattern, m_caseSensitivity);
 
-  FAST_DEBUG("KateRegExpSearch::searchRegex( " << inputRange.start().line() << ", "
-    << inputRange.start().column() << ", " << regexp.pattern() << ", " << backwards << " )");
   if (regexp.isEmpty() || !regexp.isValid() || !inputRange.isValid() || (inputRange.start() == inputRange.end()))
   {
     QVector<KTextEditor::Range> result;
@@ -470,22 +468,11 @@ QVector<KTextEditor::Range> KateRegExpSearch::search(
         // Find (and don't match ^ in between...)
         const int first = (j == forMin) ? minLeft : 0;
         const int afterLast = (j == forMax) ? maxRight : textLine.length();
-        const QString hay = textLine.left(afterLast);
-        bool found = true;
-        int foundAt;
-        uint myMatchLen;
-        if (backwards) {
-            const int lineLen = textLine.length();
-            const int offset = afterLast - lineLen - 1;
-            FAST_DEBUG("lastIndexIn(" << hay << "," << offset << ")");
-            foundAt = regexp.lastIndexIn(hay, offset);
-            found = (foundAt != -1) && (foundAt >= first);
-        } else {
-            FAST_DEBUG("indexIn(" << hay << "," << first << ")");
-            foundAt = regexp.indexIn(hay, first);
-            found = (foundAt != -1);
-        }
-        myMatchLen = found ? regexp.matchedLength() : 0;
+        const QString hay = textLine.mid(first, afterLast-first);
+        const int foundAt = backwards ? regexp.lastIndexIn(hay)
+                                      : regexp.indexIn(hay);
+        const bool found = (foundAt != -1);
+        const uint myMatchLen = found ? regexp.matchedLength() : 0;
 
       /*
       TODO do we still need this?

part/tests/regexpsearch_test.cpp
@@ -29,7 +29,21 @@ Q_DECLARE_METATYPE(KTextEditor::Range)
 
 QTEST_KDEMAIN(RegExpSearchTest, GUI)
 
+namespace QTest {
+  template<>
+  char *toString(const KTextEditor::Range &range)
+  {
+    QByteArray ba = "Range[";
+    ba += QByteArray::number(range.start().line()) + ", " + QByteArray::number(range.start().column()) + ", ";
+    ba += QByteArray::number(range.end().line())   + ", " + QByteArray::number(range.end().column());
+    ba += "]";
+    return qstrdup(ba.data());
+  }
+}
+
 #define testNewRow() (QTest::newRow(QString("line %1").arg(__LINE__).toAscii().data()))
+
+using namespace KTextEditor;
 
 RegExpSearchTest::RegExpSearchTest()
   : QObject()
@@ -162,6 +176,17 @@ void RegExpSearchTest::testReplacementCounter()
   const QString result = KateRegExpSearch::buildReplacement(pattern, QStringList(), counter);
 
   QCOMPARE(result, expected);
+}
+
+void RegExpSearchTest::testSearchBackwardInSelection()
+{
+  KateDocument doc(false, false, false);
+  doc.setText("foobar foo bar foo bar foo");
+
+  KateRegExpSearch search(&doc, Qt::CaseSensitive);
+  const Range result = search.search("foo", Range(0, 0, 0, 15), true)[0];
+
+  QCOMPARE(result, Range(0, 7, 0, 10));
 }
 
 // kate: space-indent on; indent-width 2; replace-tabs on;

part/tests/regexpsearch_test.h
@@ -42,6 +42,8 @@ class RegExpSearchTest : public QObject
 
     void testReplacementCounter_data();
     void testReplacementCounter();
+
+    void testSearchBackwardInSelection();
 };
 
 #endif

commit b8296f0ec288159f8fc2320da42c79b65098aec9

SVN commit 1115508 by beschow:
fix a bug introduced by r1115422: when a regex match was found forward, a too small starting position was returned in some cases

CCBUG: 227459

part/search/kateregexpsearch.cpp
@@ -469,9 +469,9 @@ QVector<KTextEditor::Range> KateRegExpSearch::search(
         const int first = (j == forMin) ? minLeft : 0;
         const int afterLast = (j == forMax) ? maxRight : textLine.length();
         const QString hay = textLine.mid(first, afterLast-first);
-        const int foundAt = backwards ? regexp.lastIndexIn(hay)
-                                      : regexp.indexIn(hay);
-        const bool found = (foundAt != -1);
+        const int foundAt = first + (backwards ? regexp.lastIndexIn(hay)
+                                               : regexp.indexIn(hay));
+        const bool found = (foundAt >= first);
         const uint myMatchLen = found ? regexp.matchedLength() : 0;
 
       /*
@@ -491,7 +491,7 @@ QVector<KTextEditor::Range> KateRegExpSearch::search(
           }
       */
 
-      if (found && !((j == forMax) && (static_cast<uint>(foundAt + myMatchLen) > maxRight)))
+      if (found)
       {
         FAST_DEBUG("line " << j << ": yes");
 

part/tests/regexpsearch_test.cpp
@@ -178,6 +178,17 @@ void RegExpSearchTest::testReplacementCounter()
   QCOMPARE(result, expected);
 }
 
+void RegExpSearchTest::testSearchForward()
+{
+  KateDocument doc(false, false, false);
+  doc.setText("  \\piinfercong");
+
+  KateRegExpSearch search(&doc, Qt::CaseSensitive);
+  const Range result = search.search("\\\\piinfer(\\w)", Range(0, 2, 0, 15))[0];
+
+  QCOMPARE(result, Range(0, 2, 0, 11));
+}
+
 void RegExpSearchTest::testSearchBackwardInSelection()
 {
   KateDocument doc(false, false, false);

part/tests/regexpsearch_test.h
@@ -43,6 +43,8 @@ class RegExpSearchTest : public QObject
     void testReplacementCounter_data();
     void testReplacementCounter();
 
+    void testSearchForward();
+  
     void testSearchBackwardInSelection();
 };
 